8f78f3ac08cdbd551ff767730080d02f4f722cac
dakryuchkov <dakryuchkov@phoenixit.ru>
2022-09-24 17:14:49 +0300

    add "sign in" and "sign up" pages

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const expressHbs = require('express-handlebars');
+const hbs = require('hbs');
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static('./'));
+
+app.engine('hbs', expressHbs.engine(
+    {
+       layoutsDir: 'views/layouts',
+       defaultLayout: 'layout',
+       extname: 'hbs'
+    }
+))
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+
+app.get('/', (req, res) => {
+   res.render('login.hbs', {title: 'Login'});
+});
+
+app.get('/registration', (req, res) => {
+    res.render('registration.hbs', {title: 'Registration'});
+});
+
+app.listen(PORT, () => {
+   console.log(`Мой текст в логе после запуска ${PORT}!`);
+});
